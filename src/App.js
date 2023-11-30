590d09f1d1567f103beae722bd0fd5dc9f3c2da7
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-11-30 14:08:05 -0300

    Criando Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Footer from "./components/footer"
 import Header from "./components/header"
 
 export default function App() {
   return (
     <div>
       <Header/>
+      <Footer/>
     </div>
   )
 }
