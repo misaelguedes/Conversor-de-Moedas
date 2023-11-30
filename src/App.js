24bbe9eb917783c4a4c7d8e5fb32a3144ab3a7e3
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-11-30 14:40:51 -0300

    Criando Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Footer from "./components/footer"
 import Header from "./components/header"
+import Section from "./components/section"
 
 export default function App() {
   return (
     <div>
       <Header/>
+      <Section/>
       <Footer/>
     </div>
   )
---
+++ src/components/section/index.js
@@ -0,0 +1,21 @@
+import './section.css'
+
+export default function Section() {
+    return (
+        <section>
+            <p>Selecione a moeda que dejesa converter!</p>
+            <button>REAL EM DÓLAR</button>
+            <button>REAL EM EURO</button>
+            <button>REAL EM LIBRA</button>
+            <button>DOLÁR EM REAL</button>
+            <button>DÓLAR EM EURO</button>
+            <button>DÓLAR EM LIBRA</button>
+            <button>EURO EM REAL</button>
+            <button>EURO EM DÓLAR</button>
+            <button>EURO EM LIBRA</button>
+            <button>LIBRA EM REAL</button>
+            <button>LIBRA EM DÓLAR</button>
+            <button>LIBRA EM EURO</button>
+        </section>
+    )
+}
